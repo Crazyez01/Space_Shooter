5ca6469da69dde2351bb7925f2ddc8fc27ece2dd
Alex <97403345+Crazyez01@users.noreply.github.com>
2022-06-12 20:10:17 -0300

    Pegando meu RB
    Pegando meu rigidibody2d

--- Space_Shooter/Assets/Script/Player.cs
+++ Space_Shooter/Assets/Script/Player.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D meuRB;
     // Start is called before the first frame update
     void Start()
     {
-        
+        meuRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+ 
     }
 }
