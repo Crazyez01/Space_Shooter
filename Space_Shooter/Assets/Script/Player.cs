0f9b010ab4b72088c56a551d0ff62e2b38ed533d
Alex <97403345+Crazyez01@users.noreply.github.com>
2022-06-23 10:47:20 -0300

    Movimentação
    Movimentação

--- Space_Shooter/Assets/Script/Player.cs
+++ Space_Shooter/Assets/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Rigidbody2D meurB;
+    [SerializeField] float velocidade = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
- 
+        mov();
+
+    }
+
+    private void mov()
+    {
+        //pegando o input horizontal
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector2 minhavelocidade = new Vector2(horizontal, vertical) * velocidade;
+        //passando a minha velocidade para o meu RB
+        meurB.velocity = minhavelocidade;
     }
 }
